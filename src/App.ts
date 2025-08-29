e477c15a514dc42f2164335cac42faebcdc4e17a
Shantela21 <shantelaslie@gmail.com>
2025-08-29 11:10:05 +0200

    Commit message for my changes

--- src/App.ts
+++ src/App.ts
@@ -11,12 +11,16 @@
 //     });
 
 // @ts-ignore
-import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm"
+// import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm"
+let characters: any[];
 async function fetchItems() {
   try {
     const response = await fetch("http://localhost:3000/items");
     const data = await response.json();
-    console.log(data); // This is your actual data
+    // console.log(data); // This is your actual data
+    characters = await data;
+    console.log(characters[2]);
+
   } catch (error) {
     console.log("Error:", error);
   }
